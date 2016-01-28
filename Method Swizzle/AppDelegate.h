d36feb072f17cfb080182cdb0ee4e1376e27420b
XuHuan <xuhuan@xuhuantekimac-mini.local>
2016-01-28 10:00:07 +0800

    Initial Commit

---
+++ Method Swizzle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Method Swizzle
+//
+//  Created by XuHuan on 16/1/28.
+//  Copyright © 2016年 XuHuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Method Swizzle/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Method Swizzle
+//
+//  Created by XuHuan on 16/1/28.
+//  Copyright © 2016年 XuHuan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
